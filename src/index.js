a98bba84ab1afdd3dd6b4ee0d1e9cd97094c3e4f
Darken141 <darken141@gmail.com>
2021-04-24 20:52:42 +0200

    add backend env

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   // uri: process.env.REACT_APP_BACKEND_ENDPOINT,
-  uri: "http://localhost:4000/",
+  uri: process.env.REACT_APP_BACKEND_ENDPOINT || "http://localhost:4000/",
   cache: new InMemoryCache()
 });
 
