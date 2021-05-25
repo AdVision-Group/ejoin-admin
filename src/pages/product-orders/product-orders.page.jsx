bb4acafa454aceeb9ac3e6812ba422625eaf6000
Darken141 <darken141@gmail.com>
2021-05-25 02:27:41 +0200

    update orders

--- src/pages/product-orders/product-orders.page.jsx
+++ src/pages/product-orders/product-orders.page.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import {useQuery} from '@apollo/client'
+import {GET_ORDERS} from '../../utils/queries'
 
 import {
     ProductOrdersContainer,
@@ -13,6 +16,17 @@ import {
 } from './product-orders.styles'
 
 const ProductOrderPage = () => {
+    const [orders, setOrders] = useState(null)
+    const [formItems, setFormItems] = useState(null)
+
+    const {loading, error, data} = useQuery(GET_ORDERS)
+
+    useEffect(() => {
+        if(loading) return
+        console.log(data)
+        setOrders(data.orders)
+    }, [loading, orders])
+
     return (
         <ProductOrdersContainer>
             <ProductsContainer>
@@ -20,78 +34,46 @@ const ProductOrderPage = () => {
                     {"+"}
                 </EmptyContainer>
 
-                <ProductOverviewContainer>
-                    <ImageContainer>
-                        IMG
-                    </ImageContainer>
-                    <ContentContainer>
-                        <h2>Product name</h2>
-                        <ul>
-                            <li>Item</li>
-                            <li>Item</li>
-                            <li>Item</li>
-                        </ul>
-                    </ContentContainer>
-                    <OptionsContainer>
-                        <DeligateButton>Spracovat</DeligateButton>
-                        <UpdateButton>Upravit</UpdateButton>
-                    </OptionsContainer>
-                </ProductOverviewContainer>
-             
-                <ProductOverviewContainer>
-                    <ImageContainer>
-                        IMG
-                    </ImageContainer>
-                    <ContentContainer>
-                        <h2>Product name</h2>
-                        <ul>
-                            <li>Item</li>
-                            <li>Item</li>
-                            <li>Item</li>
-                        </ul>
-                    </ContentContainer>
-                    <OptionsContainer>
-                        <DeligateButton>Spracovat</DeligateButton>
-                        <UpdateButton>Upravit</UpdateButton>
-                    </OptionsContainer>
-                </ProductOverviewContainer>
-             
-                <ProductOverviewContainer>
-                    <ImageContainer>
-                        IMG
-                    </ImageContainer>
-                    <ContentContainer>
-                        <h2>Product name</h2>
-                        <ul>
-                            <li>Item</li>
-                            <li>Item</li>
-                            <li>Item</li>
-                        </ul>
-                    </ContentContainer>
-                    <OptionsContainer>
-                        <DeligateButton>Spracovat</DeligateButton>
-                        <UpdateButton>Upravit</UpdateButton>
-                    </OptionsContainer>
-                </ProductOverviewContainer>
-             
-                <ProductOverviewContainer>
-                    <ImageContainer>
-                        IMG
-                    </ImageContainer>
-                    <ContentContainer>
-                        <h2>Product name</h2>
-                        <ul>
-                            <li>Item</li>
-                            <li>Item</li>
-                            <li>Item</li>
-                        </ul>
-                    </ContentContainer>
-                    <OptionsContainer>
-                        <DeligateButton>Spracovat</DeligateButton>
-                        <UpdateButton>Upravit</UpdateButton>
-                    </OptionsContainer>
-                </ProductOverviewContainer>
-             
+                {orders && orders.map(order => {
+                    console.log(order.orderData.product)
+                    const productObj = order.orderData.product
+                    // delete productObj["__typename"]
+
+                    const items = Object.keys(productObj).map(val => {
+                        return Object.keys(productObj[val]).filter(v => {
+                            console.log(productObj[val][v])
+                            return productObj[val][v].value
+                        }) 
+                    })
+
+                    const filteredItems = items.filter(val => val.length > 0)
+                    console.log(filteredItems)
+
+                    return (
+                        <ProductOverviewContainer key={order.id}>
+                            {/* <ImageContainer>
+                                IMG
+                            </ImageContainer> */}
+                            <ContentContainer>
+                                <h2>{order.productID}</h2>
+                                <h3>{order.status}</h3>
+                                <ul>
+                                    {
+                                        filteredItems.map((item, idx) => (
+                                            <li>{item[0]}</li>
+                                        ))
+                                    }
+                                </ul>
+                            </ContentContainer>
+                            <OptionsContainer>
+                                <DeligateButton>Spracovat</DeligateButton>
+                                <UpdateButton>Upravit</UpdateButton>
+                            </OptionsContainer>
+                        </ProductOverviewContainer>
+                    )
+                })}
+
+            
             </ProductsContainer>
         </ProductOrdersContainer>
     )
--- src/utils/queries.js
+++ src/utils/queries.js
@@ -14,3 +14,103 @@ export const GET_POSTS = gql`
         }
     }
 ` 
+
+export const GET_ORDERS = gql`
+    query {
+        orders {
+            id
+            status
+            productID
+            orderData {
+                name
+                email
+                phone
+                street
+                psc
+                city
+                country
+                isDeliveryAddressDifferent
+                deliveryStreet
+                deliveryPsc
+                deliveryCity
+                deliveryCountry
+                isBusiness
+                business {
+                    name
+                    ico
+                    dic
+                    icdph
+                }
+                product {
+                    chargingOptions {
+                        charging {
+                            value
+                            price
+                        }
+                    }
+                cabelOptions {
+                    cabel {
+                        value
+                        price
+                    }
+                }
+                positionOptions {
+                    position {
+                        value
+                        price
+                    }
+                }
+                buyOptions {
+                    buy {
+                        value
+                        price
+                    }
+                }
+                authOptions {
+                    qr {
+                        value
+                        price
+                    }
+                }
+                wifiOptions {
+                    online {
+                        value
+                        price
+                        type
+                    }
+                }
+                eleOptions {
+                    ele {
+                        value
+                        price
+                    }
+                }
+                servicesOptions {
+                    service {
+                        value
+                        price
+                    }
+                }
+                stationsOptions {
+                    stations {
+                        value
+                        price
+                    }
+                }
+                smartSolutionsOptions {
+                    smartSolutions {
+                        value
+                        price
+                    }
+                }
+                chargingNetworkOptions {
+                    charginNetwork {
+                        value
+                        price
+                    }
+                }
+            }
+        }
+    }
+}
+`
