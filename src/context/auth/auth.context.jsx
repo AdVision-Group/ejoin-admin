fd27df5dc38db02991471c28a0d3360c8bb82396
Darken141 <darken141@gmail.com>
2021-06-19 13:12:11 +0200

    update

--- src/context/auth/auth.context.jsx
+++ src/context/auth/auth.context.jsx
@@ -1,64 +1,79 @@
-import React, { createContext, useState, useEffect, useContext } from 'react'
-import {client} from '../../index'
-import jwt_decode from 'jwt-decode'
+import React, { createContext, useState, useEffect, useContext } from "react"
+import { client } from "../../index"
+import jwt_decode from "jwt-decode"
 
 export const AuthContext = createContext({
-    userID: null,
-    token: null,
-    currentUser: null,
-    isAuthenticated: false,
-    isAdmin: false,
-    isEditor: false,
-    getToken: () => { },
-    logout: () => { }
+	userID: null,
+	token: null,
+	currentUser: null,
+	isAuthenticated: false,
+	isAdmin: false,
+	isEditor: false,
+	getToken: () => {},
+	logout: () => {},
 })
 
 export const useAuthContext = () => useContext(AuthContext)
 
 const AuthProvider = ({ children }) => {
-    const [token, setToken] = useState(localStorage.getItem("accessToken") || null)
-    const [currentUser, setCurrentUser] = useState(null)
-    const [userID, setUserID] = useState(localStorage.getItem("userId") || null)
-    const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem("accessToken") ? true : false)
+	const [token, setToken] = useState(
+		localStorage.getItem("accessToken") || null
+	)
+	const [currentUser, setCurrentUser] = useState(null)
+	const [userID, setUserID] = useState(localStorage.getItem("userId") || null)
+	const [isAuthenticated, setIsAuthenticated] = useState(
+		localStorage.getItem("accessToken") ? true : false
+	)
 
-    const getToken = (token) => {
-        setToken(token)
-        const user = jwt_decode(token)
-        console.log(user)
-        setUserID(user.sub)
-        localStorage.setItem("userId", user.sub)
-        localStorage.setItem("accessToken", token)
-        setIsAuthenticated(true)
-        client.resetStore()
-    }
+	const getToken = (token) => {
+		setToken(token)
+		const user = jwt_decode(token)
+		console.log(user)
+		setUserID(user.sub)
+		localStorage.setItem("userId", user.sub)
+		localStorage.setItem("accessToken", token)
+		setIsAuthenticated(true)
+		client.resetStore()
+	}
 
-    const logout = (callback = () => {}) => {
-        localStorage.removeItem("accessToken")
-        localStorage.removeItem("userId")
-        setCurrentUser(null)
-        setIsAuthenticated(false)
-        setUserID(null)
-        callback()
-        client.resetStore()
-    }
+	const logout = (callback = () => {}) => {
+		localStorage.removeItem("accessToken")
+		localStorage.removeItem("userId")
+		setCurrentUser(null)
+		setIsAuthenticated(false)
+		setUserID(null)
+		callback()
+		client.resetStore()
+	}
 
-    // console.log(token)
-    // console.log(isAuthenticated)
+	useEffect(() => {
+		if (!token) return
+		console.log("TOKEN VERIFICATION START")
+		const dToken = jwt_decode(token)
+		if (Date.now() >= dToken.exp * 1000) {
+			console.log("TOKEN IS EXPIRED")
+			return logout()
+		}
+		console.log("TOKEN VERIFICATION END")
+	}, [token])
 
-    return (
-        <AuthContext.Provider
-            value={{
-                userID,
-                token,
-                currentUser,
-                isAuthenticated,
-                getToken,
-                logout
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
+	// console.log(token)
+	// console.log(isAuthenticated)
+
+	return (
+		<AuthContext.Provider
+			value={{
+				userID,
+				token,
+				currentUser,
+				isAuthenticated,
+				getToken,
+				logout,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	)
 }
 
 export default AuthProvider
